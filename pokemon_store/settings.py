f0df3b7a876a39e9e4dadfe2e3ce3b4287ad4278
francoxr <franco.roca.pe@gmail.com>
2021-07-31 21:21:21 -0500

    add django_heroku

--- pokemon_store/settings.py
+++ pokemon_store/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -170,3 +171,4 @@ CORS_ALLOWED_ORIGINS = []
 #     'POST',
 #     'PUT',
 # ]
+django_heroku.settings(locals())
