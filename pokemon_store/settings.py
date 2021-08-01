32c851bcdcc43955af62e7ef36c6a04ed8265dd6
francoxr <franco.roca.pe@gmail.com>
2021-07-31 20:55:05 -0500

    add comma

--- pokemon_store/settings.py
+++ pokemon_store/settings.py
@@ -146,7 +146,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfile")
 STATIC_URL = "/static/"
 # *
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
 )
 # *
 STATICFILE_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
